79107d5fa5189b0b91ccadf06c05d6451ed8bd4c
BHATTU SAI PRANEETH <149008961+Bhattu-Sai-Praneeth@users.noreply.github.com>
2025-09-25 12:08:52 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'tier3db.cto0amyqs6dc.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root12345',
+    DB_DATABASE : 'webappdb'
+});
